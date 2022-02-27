9c40a639be97ba877ec34dc9102b9ce8d1c56b37
jester <jaimebuddy98@gmail.com>
2022-02-26 22:28:11 -0600

    added a logout route for users

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -29,3 +29,15 @@ router.post('/login', (req, res) => {
       });
     });
 });
+
+// DESTROY CURRENT SESSION | LOGOUT
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+        res.status(204).end();
+        });
+    }
+    else {
+        res.status(404).end();
+    }
+});
