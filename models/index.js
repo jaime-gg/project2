69756c262b5dda809d446aed8397f0faf1ed5301
jester <jaimebuddy98@gmail.com>
2022-02-26 21:32:36 -0600

    added necessary associations

--- models/index.js
+++ models/index.js
@@ -5,3 +5,30 @@ const Comment = require('./Comment');
 
 
 // ASSOCIATIONS
+User.hasMany(Story, {
+    foreignKey: 'user_id'
+});
+  
+Story.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+  
+Comment.belongsTo(Story, {
+    foreignKey: 'story_id'
+});
+  
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+Story.hasMany(Comment, {
+    foreignKey: 'story_id'
+});
+
+module.exports = {User, Comment, Story }
