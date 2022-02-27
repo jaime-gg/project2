270ba723f36b7f2bb2f060029099609bf6af805d
jester <jaimebuddy98@gmail.com>
2022-02-27 10:55:45 -0600

    post route for new story

--- controllers/api/story-routes.js
+++ controllers/api/story-routes.js
@@ -1,5 +1,19 @@
 const router = require('express').Router();
 const { Story } = require("../../models");
 
+// CREATE A NEW STORY | /API/STORIES
+router.post('/', (req, res) => {
+    // RUN CREATE METHOD
+    Story.create({
+        user_id: req.session.user_id,
+        title: req.body.title,
+        body: req.body.body
+    })
+    .then(dbStoryData => res.json(dbStoryData))
+    .catch(err => {
+        res.status(500).json(err);
+        console.log(err);
+    });
+});
 
 module.exports = router;
