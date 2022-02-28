bf4c1ceca6dcce3db2a567a0128afa2d5b346124
dit <ditazanelli@gmail.com>
2022-02-28 16:55:41 -0600

    relevant info regarding the logic of uploading pf sketch image to db

--- book-cover/cover.js
+++ book-cover/cover.js
@@ -38,3 +38,14 @@ function uploadCover() {
 //capture the users bg color #, text color #, text size, and text content when save button is pressed
 
 //use router post to save() p5 sketch with the vairables above into a database connected to story content +user info
+
+///////////////
+
+// from the p5.js issue "Save( ) function save on server #2733"
+// https://github.com/processing/p5.js/issues/2733
+
+// " p5.js is a client side library meaning that it runs on the browser and thus will have no knowledge of the structure/architecture of the server, it doesn't know it is served from the script folder for example so it won't know how to "save" any file.
+
+// An alternative solution is to prepare an endpoint on your server (which means you need to write some backend code) which receive this data from the client. Then the client just need to send the data, in p5.js using something like httpPost, then the server can save the file in the right place. Do ask on the forum if you need help with this. :wink"
+
+//////////////
