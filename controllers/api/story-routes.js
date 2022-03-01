692e4b719dec3fa9afb2ac0080263c5ca2422f31
jester <jaimebuddy98@gmail.com>
2022-02-28 19:42:24 -0600

    reformatted according to eslint

--- controllers/api/story-routes.js
+++ controllers/api/story-routes.js
@@ -1,84 +1,84 @@
 const router = require('express').Router();
-const { User, Story, Comment } = require("../../models");
+const { User, Story, Comment } = require('../../models');
 
 
 // GET STORY ROUTES ----------------------------------------------------------------------------------------------------------------------------
 
 // GET ALL STORIES | /API/STORIES
 router.get('/', (req, res) => {
-    // RUN FIND ALL METHOD
-    Story.findAll({
-        // NEWER STORY AT THE TOP OF PAGE 
-        order: [['created_at', 'DESC']],
-        attributes: [
-            'id',
-            'body',
-            'title',
-            'created_at'
-        ],
-        // PULL INFO FROM OTHER TABLES/MODELS
-        include: [
-            {
-                model: Comment,
-                attributes: ['id', 'comment_text', 'story_id', 'user_id', 'created_at'],
-                include: {
-                    model: User,
-                    attributes: ['username']
-                }
-            },
-            {
-                model: User,
-                attributes: ['username']
-            }
-        ]
-    })
+  // RUN FIND ALL METHOD
+  Story.findAll({
+    // NEWER STORY AT THE TOP OF PAGE
+    order: [['created_at', 'DESC']],
+    attributes: [
+      'id',
+      'body',
+      'title',
+      'created_at'
+    ],
+    // PULL INFO FROM OTHER TABLES/MODELS
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'story_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
     .then(dbStoryData => res.json(dbStoryData))
     .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+      console.log(err);
+      res.status(500).json(err);
     });
 });
 
 // GET A SINGLE STORY | /API/STORIES/:ID
 // COULD BE USEFUL FOR VIEW-AUTHOR-ROUTES
 router.get('/:id', (req, res) => {
-    // RUN FIND_ONE METHOD 
-    Story.findOne({
-        where: {
-            id: req.params.id
-        },
-        attributes: [
-            'id',
-            'body',
-            'title',
-            'created_at'
-        ],
-        // PULL DATA FROM COMMENT AND USER MODELS
-        include: [
-            {
-                model: Comment,
-                attributes: ['id', 'comment_text', 'story_id', 'user_id', 'created_at'],
-                include: {
-                    model: User,
-                    attributes: ['username']
-                }
-            },
-            {
-                model: User,
-                attributes: ['username']
-            }
-        ]
-    })
-    .then(dbStoryData => {
-        if (!dbStoryData) {
-          res.status(404).json({ message: 'No story found with this id' });
-          return;
+  // RUN FIND_ONE METHOD
+  Story.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'body',
+      'title',
+      'created_at'
+    ],
+    // PULL DATA FROM COMMENT AND USER MODELS
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'story_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
         }
-        res.json(dbStoryData);
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbStoryData => {
+      if (!dbStoryData) {
+        res.status(404).json({ message: 'No story found with this id' });
+        return;
+      }
+      res.json(dbStoryData);
     })
     .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+      console.log(err);
+      res.status(500).json(err);
     });
 });
 
@@ -86,65 +86,65 @@ router.get('/:id', (req, res) => {
 
 // CREATE A NEW STORY | /API/STORIES
 router.post('/', (req, res) => {
-    // RUN CREATE METHOD
-    Story.create({
-        user_id: req.session.user_id,
-        title: req.body.title,
-        body: req.body.body
-    })
+  // RUN CREATE METHOD
+  Story.create({
+    user_id: req.session.user_id,
+    title: req.body.title,
+    body: req.body.body
+  })
     .then(dbStoryData => res.json(dbStoryData))
     .catch(err => {
-        res.status(500).json(err);
-        console.log(err);
+      res.status(500).json(err);
+      console.log(err);
     });
 });
 
 // DELETE STORY | LOGIN REQUIRED | /API/STORIES/:ID
 router.delete('/:id', withAuth, (req, res) => {
-    // RUN DESTROY FUNCTION USING INPUTTED ID
-    Story.destroy({
-        where: {
-            id: req.params.id
-        }
-    })
+  // RUN DESTROY FUNCTION USING INPUTTED ID
+  Story.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
     .then(dbStoryData => {
-        if (!dbStoryData) {
-            res.status(404).json({ message: 'No story found with this id' });
-            return;
-        }
-        res.json(dbStoryData);
+      if (!dbStoryData) {
+        res.status(404).json({ message: 'No story found with this id' });
+        return;
+      }
+      res.json(dbStoryData);
     })
     .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+      console.log(err);
+      res.status(500).json(err);
     });
 });
 
 // EDIT STORY | LOGIN REQUIRED | /API/STORIES/:ID
 router.put('/:id', withAuth, (req, res) => {
-    Story.update(
-        // ADD NEW/EDITED TITLE AND BODY 
-        {
-            title: req.body.title,
-            body: req.body.body
-        },
-        // USE INPUTTED ID 
-        {
-            where: {
-                id: req.params.id
-            }
-        }
-    )
+  Story.update(
+    // ADD NEW/EDITED TITLE AND BODY
+    {
+      title: req.body.title,
+      body: req.body.body
+    },
+    // USE INPUTTED ID
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
     .then(dbStoryData => {
-        if (!dbStoryData) {
-            res.status(404).json({ message: 'No story found with this id' });
-            return;
-        }
-        res.json(dbStoryData);
+      if (!dbStoryData) {
+        res.status(404).json({ message: 'No story found with this id' });
+        return;
+      }
+      res.json(dbStoryData);
     })
     .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+      console.log(err);
+      res.status(500).json(err);
     });
 });
 
--- models/Comment.js
+++ models/Comment.js
@@ -7,43 +7,43 @@ class Comment extends Model {}
 
 // CREATE THE FIELDS/COLUMNS FOR COMMENT MODEL
 Comment.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        }, 
-        comment_text: {
-            type: DataTypes.TEXT,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
-        },
-        // PULL DATA FROM THE USER MODEL | USER ID
-        user_id: {
-            type: DataTypes.INTEGER,
-            references: {
-            model: 'user',
-            key: 'id'
-            }
-        },
-        // PULL DATA FROM THE STORY MODEL | STORY ID
-        story_id: {
-            type: DataTypes.INTEGER,
-            references: {
-            model: 'story',
-            key: 'id'
-            }
-        }
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
     },
-    {
-        sequelize,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'comment'
+    comment_text: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    // PULL DATA FROM THE USER MODEL | USER ID
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    },
+    // PULL DATA FROM THE STORY MODEL | STORY ID
+    story_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'story',
+        key: 'id'
+      }
     }
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment'
+  }
 );
 
 module.exports = Comment;
