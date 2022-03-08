8aeacd030e4a9b61c0258ccf1cc713fecf3c9c04
jester <jaimebuddy98@gmail.com>
2022-03-08 13:10:39 -0600

    more naming

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const sess = {
 app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'Public')));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
